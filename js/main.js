01881ab81f01a6089ac81c0b51c328befdccc0ae
Kateryna Safonova <katrin.safonowa@gmail.com>
2022-02-12 10:42:32 +0200

    warunki

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let name = 'Warunki';
+
+console.log(name);
+
+let number = 8;
+
+
+
+if (number>10) {
+    console.log('Podana liczba jest większa od 10'); 
+} 
+ else{
+    console.log('Podana liczba nie jest większa od 10');
+}
+
